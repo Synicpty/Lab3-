14bb4b3f35494040bb8a89cb5a0edcfd47ef52e3
Synicpty <112263888+Synicpty@users.noreply.github.com>
2022-10-04 08:39:17 -0500

    Add files via upload

---
+++ lab3.com/index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Laboratorio 3 </title>
+</head>
+
+
+<body>
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <img src="logo.png" class="img-fluid" alt="...">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Catalogo</a>
+                        </li>
+                        
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categorias
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">I. Sala</a></li>
+                                <li><a class="dropdown-item" href="#">II. Habitacion</a></li>
+                                <li><a class="dropdown-item" href="#">III. Comedor</a></li>
+                                <li><a class="dropdown-item" href="#">IV. Niños</a></li>
+                                <li><a class="dropdown-item" href="#">V. Baños</a></li>
+                                <li><a class="dropdown-item" href="#">VI. Cocina</a></li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Texto Buscado" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="slide1.jpg" class="d-block w-100" alt="Foto1" width="1280">
+            </div>
+            <div class="carousel-item">
+                <img src="slide2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="slide3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="container-sm">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+
+                <div class="card border-light mb-3">
+                <center><img src="top.png"  width="150px" class="img-fluid" alt="..." ></center>
+                <h3 align="center"></i>Nosotros</h3>
+                <p>Some quick example text to build on the card title and make up the bulk 
+                    of the cards content. Some quick example text to build on the card title and make up the bulk 
+                    of the cards content.</p>
+                    <button type="button" class="btn btn-success">Ver Detalles</button>
+                    
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <div class="card border-light mb-3">
+                <center><img src="top.png"  width="150px" class="img-fluid" alt="..." ></center>
+                <h3 align="center"></i>Nosotros</h3>
+                <p>Some quick example text to build on the card title and make up the bulk 
+                of the cards content. Some quick example text to build on the card title and make up the bulk 
+                of the cards content.</p>
+                <button type="button" class="btn btn-success" >Ver Detalles</button>
+        
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                   
+                   <div class="card border-light mb-3">
+                   <center><img src="top.png"  width="150px" class="img-fluid" alt="..."></center>
+                   <h3 align="center"></i>Nosotros</h3>
+                   <p>Some quick example text to build on the card title and make up the bulk 
+                   of the cards content. Some quick example text to build on the card title and make up the bulk 
+                   of the cards content.</p>
+                   <button type="button" class="btn btn-success" >Ver Detalles</button>
+           
+                   </div>
+                       </div>
+              
+            </div>
+        </div>
+    </div>
+
+
+<div><h1 class= "text-center">Catalogo</h1></div>
+    
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="producto.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                            <h4 font face="Product Sans">$10.50</h4>
+                            <hr>
+                            <div class="btn-group" role="group" aria-label="Basic example" style="width:100%">
+                                <button type="button" class="btn btn-dark" width="250px">Ver</button>
+                            
+                                <button type="button" class="btn btn-dark" width="250px">Comprar</button>
+                            </div>
+                    </div>
+                </div>
+
+                
+            </div>
+    </div>
+
+
+
+<div class="row ${1| ,row-cols-2,row-cols-3, auto,justify-content-md-center,|}">
+    
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
